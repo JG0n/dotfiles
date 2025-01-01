98e2410176cb2e5a851f7d1cb66a9fcb65b0476b
JG0n <EduardoGoncalves98@protonmail.com>
2025-01-01 15:55:54 +0000

    Change new winsow behaviour to inherit

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -11,8 +11,11 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.api.nvim_set_keymap('n', '<esc>', ':noh<return>', {noremap = true})
+
+-- Copy/Paste
 vim.api.nvim_set_keymap('n', '<leader>y', '"+y', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>p', '"+p', {noremap = true})
+
 vim.api.nvim_set_keymap('v', '<leader>y', '"+y', {noremap = true})
 vim.api.nvim_set_keymap('v', '<leader>p', '"+p', {noremap = true})
 
