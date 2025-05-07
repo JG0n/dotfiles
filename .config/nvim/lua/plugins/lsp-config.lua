9360e3d8f2f31f20822065f3e67aed6ba9040e92
JG0n <EduardoGoncalves98@protonmail.com>
2025-05-07 23:17:15 +0100

    update to new lspconfig version

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -66,92 +66,77 @@ return {
 				end
 			end
 
-			local handlers = {
-				function(server_name)
-					lspconfig[server_name].setup({
-						capabilities = capabilities,
-						on_attach = function(client, bufnr)
-							lsp_formatting(client, bufnr)
-						end,
-					})
-				end,
-				["ts_ls"] = function()
-					lspconfig.ts_ls.setup({
-						capabilities = capabilities,
-						on_attach = function(client, bufnr)
-							-- vim.keymap.set('n', '<leader>gpf', ":! npx prettier % --write <CR>",
-							-- 	{ desc = 'Run prettier on current file' })
-							vim.keymap.set('n', '<leader>gpp', ":! npx prettier . --write <CR>",
-								{ desc = 'Run prettier in project' })
-							vim.keymap.set('n', '<leader>gpf', ":Prettier<CR>",
-								{ desc = 'Run prettier on current file' })
-
-							ts_formatting(client, bufnr)
-						end,
-						commands = {
-							OrganizeImports = {
-								ts_organize_imports,
-								description = "Organize Imports",
-							},
-						},
-					})
-				end,
-				['lua_ls'] = function()
-					lspconfig.lua_ls.setup({
-						capabilities = capabilities,
-						on_attach = function(client, bufnr)
-							lsp_formatting(client, bufnr)
-						end,
-						settings = {
-							Lua = {
-								runtime = {
-									version = "LuaJIT",
-								},
-								diagnostics = {
-									globals = { "vim" },
-								},
-								workspace = {
-									library = vim.api.nvim_get_runtime_file("", true),
-								},
-								telemetry = {
-									enable = false,
-								},
-							}
-						},
-					})
-				end,
-			}
-
-			lspconfig.csharp_ls.setup({
+			vim.lsp.config("*", {
 				capabilities = capabilities,
 				on_attach = function(client, bufnr)
-					local params = util.make_formatting_params({})
+					lsp_formatting(client, bufnr)
+				end
+			})
+
+			vim.lsp.config["csharp_ls"] = {
+				capabilities = capabilities,
+				on_attach = function()
 					vim.api.nvim_create_autocmd("BufWritePre", {
 						callback = function()
 							-- client.request('textDocument/formatting', params, nil, bufnr)
-							vim.lsp.buf.format { async = false, id = params.client_id }
+							vim.lsp.buf.format { async = false }
 						end,
 					})
-				end,
-				handlers = {
-					["textDocument/definition"] = require('csharpls_extended').handler,
-					["textDocument/typeDefinition"] = require('csharpls_extended').handler,
+				end
+			}
+			require("csharpls_extended").buf_read_cmd_bind()
+
+
+			vim.lsp.config["lua_ls"] = {
+				settings = {
+					Lua = {
+						runtime = {
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+						telemetry = {
+							enable = false,
+						},
+					}
 				},
-			})
+			}
+
+			vim.lsp.config["ts_ls"] = {
+				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					-- vim.keymap.set('n', '<leader>gpf', ":! npx prettier % --write <CR>",
+					-- 	{ desc = 'Run prettier on current file' })
+					vim.keymap.set('n', '<leader>gpp', ":! npx prettier . --write <CR>",
+						{ desc = 'Run prettier in project' })
+					vim.keymap.set('n', '<leader>gpf', ":Prettier<CR>",
+						{ desc = 'Run prettier on current file' })
+
+					ts_formatting(client, bufnr)
+				end,
+				commands = {
+					OrganizeImports = {
+						ts_organize_imports,
+						description = "Organize Imports",
+					},
+				},
+			}
 
 			require("mason-lspconfig").setup({
-				handlers = handlers,
-				automatic_installation = false,
 				ensure_installed = {
 					"lua_ls",
 					"gopls",
 					"basedpyright",
 					"ts_ls", "eslint",
 					"hyprls",
-					-- "csharp_ls",
 					"html", "emmet_ls", "cssls", "tailwindcss",
-					"omnisharp",
-				}
+					"csharp_ls",
+				},
+				automatic_enable = true
 			})
 		end
 	}
