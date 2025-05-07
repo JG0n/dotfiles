66469c68cf2a2624a925f4b1c5fdd09c9c81afe8
JG0n <EduardoGoncalves98@protonmail.com>
2025-05-07 22:15:59 +0100

    neogen config

--- .config/nvim/lua/plugins/neogen.lua
+++ .config/nvim/lua/plugins/neogen.lua
@@ -1,6 +1,20 @@
 return {
 	"danymat/neogen",
-	config = true,
+	-- config = true,
 	-- Uncomment next line if you want to follow only stable versions
 	-- version = "*"
+	config = function ()
+		require('neogen').setup {
+			enabled = true,             --if you want to disable Neogen
+			input_after_comment = true, -- (default: true) automatic jump (with insert mode) on inserted annotation
+			-- jump_map = "<C-e>"       -- (DROPPED SUPPORT, see [here](#cycle-between-annotations) !) The keymap in order to jump in the annotation fields (in insert mode)
+			languages = {
+				cs = {
+					template = {
+						annotation_convention = "xmldoc"
+					}
+				}
+			}
+		}
+	end
 }
