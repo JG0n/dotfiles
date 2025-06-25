612f8a31efbcbc0b2e4fa969a3be982075776d95
JG0n <EduardoGoncalves98@protonmail.com>
2025-06-25 17:49:43 +0100

    color changes to the background of kitty and nvim

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,8 +1,24 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme "catppuccin"
-    end
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		mocha = {
+			base = "#000000",
+			mantle = "#000000",
+			crust = "#000000",
+		},
+	},
+	config = function()
+		require("catppuccin").setup({
+			color_overrides = {
+				mocha = {
+					base = "#11111b",
+				},
+			},
+		})
+
+		-- setup must be called before loading
+		vim.cmd.colorscheme "catppuccin"
+	end
 }
